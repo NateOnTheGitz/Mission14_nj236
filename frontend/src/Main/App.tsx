c52563cb99fec78c7ae357b5023f311efe03cafb
Nathan Johnson <nj236@byu.edu>
2023-04-10 18:23:09 -0600

    add api

---
+++ frontend/src/Main/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header slogan="Joel Hilton Movie Site - Main" />
+    </div>
+  );
+}
+
+export default App;
