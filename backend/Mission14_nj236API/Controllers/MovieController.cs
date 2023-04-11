77b2d3cc9c5e39c45add0b7e25a98ddcb91b4432
Nathan Johnson <nj236@byu.edu>
2023-04-10 21:01:00 -0600

    working api

---
+++ backend/Mission14_nj236API/Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission14_nj236API.Data;
+
+namespace Mission14_nj236API.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class MovieController : Controller
+    {
+        private JoelHiltonMovieCollectionContext context;
+        public MovieController(JoelHiltonMovieCollectionContext temp) {
+            context = temp;
+        }
+        public IEnumerable<Movie> Get()
+        {
+            return context.Movies
+                .Where(x => x.Edited == "Yes")
+                .ToArray();
+        }
+    }
+}
